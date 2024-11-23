e6f883d835e4865e6bf56cd158dd078d94feb1eb
Dmytro Varchuk <loltop792@gmail.com>
2024-11-23 16:16:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+def calculate(expression):
+    result = 0
+    current_number = 0
+    operator = '+'
+
+    for char in expression:
+        if char.isdigit():
+            current_number = current_number * 10 + int(char)
+        elif char in '+-*/':
+            if operator == '+':
+                result += current_number
+            elif operator == '-':
+                result -= current_number
+            elif operator == '*':
+                result *= current_number
+            elif operator == '/':
+                result / current_number
+            operator = char
+            current_number = 0
+
+    if operator == '+':
+        result += current_number
+    elif operator == '-':
+        result -= current_number
+    elif operator == '*':
+        result *= current_number
+    elif operator == '-':
+        result /= current_number
+
+    print(result)
+if __name__ == '__main__':
+    _expression = input()
+    calculate(_expression)
+
