d0d48578eb560387e10e2a92e4ec330b7b9c540c
Dmytro Varchuk <loltop792@gmail.com>
2024-11-23 16:21:35 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def reverse(row):
+    reversed_row = []
+    for i in range(len(row) - 1, -1, -1):
+        reversed_row.append(row[i])
+    print("Row:", row)
+    print("Reversed row:", reversed_row)
+
+
+if __name__ == '__main__':
+    _row = []
+    while True:
+        element = input("Fill up your row (enter to finish): ")
+        if element == "":
+            break
+        _row.append(element)
+    reverse(_row)
+
