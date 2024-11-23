52ae31094cdfee59ae460882d611d8642e84812e
Dmytro Varchuk <loltop792@gmail.com>
2024-11-23 16:23:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+def number_of_letters_and_numbers(row):
+    numbers_value = 0
+    letters_value = 0
+    for element in row:
+        if element.isalpha():
+            letters_value += len(element)
+        elif element.isdigit():
+            num = int(element)
+            if num < 9:
+                numbers_value += 1
+            else:
+                while num > 9:
+                    numbers_value += 1
+                    num = num // 10
+                numbers_value += 1  # for the last digit
+    print(f"letters value{letters_value}")
+    print(f"numbers_value{numbers_value}")
+
+if __name__ == '__main__':
+    _row = []
+    while True:
+        element = input("Fill up your row : ")
+        if element == "":
+            break
+        _row.append(element)
+    number_of_letters_and_numbers(_row)
