89a06f57693e65313ce974030db6a3e1e51d8651
Dmytro Varchuk <loltop792@gmail.com>
2024-11-23 16:19:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+def main():
+    n = 10
+    numbers = [random.randint(-10, 10) for _ in range(n)]
+    numbers.sort()
+    negative_elements = 0
+    zero_elements = 0
+    positive_elements = 0
+
+    for element in numbers:
+        if element < 0:
+            negative_elements +=1
+        elif element == 0:
+            zero_elements += 1
+        else:
+            positive_elements += 1
+
+    print(f"Negative elements: {negative_elements}")
+    print(f"Zero elements: {zero_elements}")
+    print(f"Positive elements: {positive_elements}")
+if __name__ == '__main__':
+    main()
+
