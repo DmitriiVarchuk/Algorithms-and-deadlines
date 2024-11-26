8f009cd31f60173cc72b7754f6a61ed6d0fce2b2
Dmytro Varchuk <loltop792@gmail.com>
2024-11-26 17:46:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+def number_of_word(row, symbol):
+    count = 0
+    symbol = symbol.lower()
+    for i in range(len(row)):
+        for char in row[i].lower():
+            if char == symbol:
+                count += 1
+    print(f"{symbol} {count} times in the row")
+
+if __name__ == '__main__':
+    _row = []
+    while True:
+        element = input("Fill up your row (enter to finish): ")
+        if element == "":
+            break
+        _row.append(element)
+    _symbol = input("symbol you want to count: ")[0]
+    number_of_word(_row, _symbol)
